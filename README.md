95643fb634c0948e044dc990ac87dfa98e4a8288
Fitorec <chanerec@gmail.com>
2012-07-19 13:35:03 -0500

    Primer commit

--- README.md
+++ README.md
@@ -1,4 +1,101 @@
-sitios-joomla
+sitiosJoomla
 =============
 
-Un alias para el git, que nos ayuda a actualizar sitios Joomla!
+> Un _simple alias para el git_, que nos ayuda a actualizar sitios Joomla!
+
+**sitiosJoomla!** es un script que nos facilita la tarea de trabajar con sitios desarrollados con __Joomla!__ en repositorios administrados con **git**.
+
+
+## Instalación
+
+### Obteniendo el proyecto
+
+Lo primero que tienes que hacer es descargar el proyecto y ubicado en una parte donde lo puedas acceder fácilmente, p.e:
+
+	/home/<user>/proyectos_web/sitios-joomla/
+
+Donde **`<user>`** es nuestro usuario, en mi caso @fitorec quedaria de la suguiente forma:
+
+	/home/fitorec/proyectos_web/sitios-joomla/
+
+
+### Configurando alias.
+
+Vamos agregar un alias "**sitio-joomla**" esto es similar a agregar otro "**sub-comando** al git", es decir podremos ejecutar:
+
+
+	git sitio-joomla -<opción>
+
+Lo hace el script cuando se ejecuta por primera vez:
+
+	bash /home/fitorec/proyectos_web/sitios-joomla/sitioJoomla.sh
+
+¿Como lo hace?, agrega un alias hacia donde apunta nuestro script, esto tambien lo puedes hacer tu mismo con la instrucción, el script solo te facilita la configuración:
+
+	git config  --global alias.sitio-joomla  '!bash /home/fitorec/proyectos_web/sitios-joomla/sitioJoomla.sh'
+
+### Configuracion general SQL
+
+#### Ruta de nuestro cliente mySQL
+
+Tal vez estes usando un entorno de desarrollo ageno al la configuración de tu S.O. por ejemplo tal ves puedas estar usando [lampp](http://www.apachefriends.org/en/xampp-linux.html), entonces la configuracion de tu cliente mysql puede estar en otra parte, por ejemplo:
+
+	/opt/lammp/etc/bin/mysql
+
+De ser asi tenemos que indicarle al script la ruta de nuestro cliente Mysql a usar, esto lo podemos hacer con la siguiente instrucción:
+	
+	git config  --global joomla.mysql-client
+
+**Nota:** Si tu cliente mysql es el mismo que el que nos devuelve el comando `which mysql` no es necesario configurar nada.
+
+#### Acceso al administrador MSQL
+
+
+Para instalar/borrar la bases de datos y usuarios automaticamente, es necesario agregar usuario/password administrador de nuestro mysql p.e:
+	
+		 git config --global joomla.mysql-admin 'root'
+		 git config --global joomla.mysql-password 'libertad'
+
+## Uso
+
+Supongamos que ya tenemos configurado el alias y que tenemos un proyecto joomla! administrado con **git** p.e. `/home/proyectos/site-x`, por otra parte los archivos los tengo ubicados en `/var/www/site-x`.
+
+Lo primero que tenemos que hacer es indicarle nuestro script que el directorio destino sera `/var/www/site-x`, para esto configuramos **localmente** el directorio destino de la siguiente manera:
+
+```sh
+	#Indicamos que /var/www/site-x es el directorio destino
+	git config joomla.dir-dst /var/www/site-x
+```
+
+Bien ahora ya tienes todo configurado!, lo siguiente sera instalar la aplicación en tu servidor local, esto significa que los archivos que tienes en tu repositorio local `/home/proyectos/site-x` se pasaran a `/var/www/site-x`.
+
+Por otra parte localmente te extrae la configuración del archivo `/var/www/site-x/` te crea el usuario/base de datos y te la carga, con un simple:
+
+	git sitio-joomla -instalar
+
+
+## Algunos Ejemplos de uso
+
+Creando la Base de datos, su usuario, la ruta de archivos, etc...:
+
+	git sitio-joomla -instalar
+
+Actualizando Simple(archivos sin seguimiento y modificados en el ultimo commit):
+
+	git sitio-joomla
+
+Actualizando base de datos:
+
+	git sitio-joomla -db
+
+Actualizando los archivos sin seguimiento y modificados hace 2 commits:
+
+	git sitio-joomla -c2
+
+Actualizando base de datos, archivos sin seguimiento y modificados hace 5 commits:
+
+	git sitio-joomla -bd -c5
+
+Esta página de ayuda:
+
+	git sitio-joomla -ayuda
---
+++ sitioJoomla.sh
@@ -0,0 +1,281 @@
+#!/bin/bash
+#Descripcion:  Utileria para manejar sitios Joomla desde el CLI.
+#Author     :  fitorec
+#                           _           _
+# _ __ ___  _   _ _ __   __| | ___  ___(_) ___ __ _   ___ ___  _ __ ___
+#| '_ ` _ \| | | | '_ \ / _` |/ _ \/ __| |/ __/ _` | / __/ _ \| '_ ` _ \
+#| | | | | | |_| | | | | (_| | (_) \__ \ | (_| (_| || (_| (_) | | | | | |
+#|_| |_| |_|\__,_|_| |_|\__,_|\___/|___/_|\___\__,_(_)___\___/|_| |_| |_|
+##########################################################################################
+cyan='\e[0;36m'
+light='\e[1;36m'
+red="\e[0;31m"
+yellow="\e[0;33m"
+white="\e[0;37m"
+end='\e[0m'
+ 
+if [ "$(git config --global alias.sitio-joomla)" = "" ]
+then
+	echo -en "${light}Congigurando el Alias${end} ";
+	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	git config  --global alias.sitio-joomla  "'!bash $DIR/sitioJoomla.sh'"
+	if [ $? -eq 0 ]
+	then
+		echo -e "<- ${white}Se creo satisfactoriamente${end}";
+	else
+		echo "<- ${red}No se pudo crear${end}";
+	fi
+	exit 0;
+fi
+
+export REPO_PATH=$(pwd);
+export MYSQL_CLIENT=$(git config joomla.mysql-client);
+if [ "$?" -eq "0" ]
+then
+	MYSQL_CLIENT=$(which mysql);
+else
+	echo "${light}Configuración especial sobre su cliente mysql:${end}"
+	echo -e "${white}${MYSQL_CLIENT}${end}"
+	exit 0
+fi
+export	JOOMLA_DST=`git config joomla.dir-dst`;
+if [ "${JOOMLA_DST}" = "" ]
+then
+	echo -e "${red}Error: Debe de configurar su directorio destino Donde se moveran los archivos"
+	echo -e "${end}Por favor ejecute ${light}git sitio-joomla -ayuda${end} para obtener mas información"
+	exit 0;
+fi
+##########################################################################################
+# Descricipcion: Extrae la información de la base de datos del JOOMLA                    #
+# Recibe como argumento el nombre del parametro al que le va extraer la información      #
+##########################################################################################
+function extractParamBD()
+{
+	cat "${REPO_PATH}/configuration.php" | grep -Eo "^\s+public\s*.$1\s+=\s+'.*'" \
+	| sed -re "s/.*=\s*//g;s/'//g" | head -1;
+}
+
+function	installBD()
+{
+	echo -e "${light}Creando Jaula SQL${end}";
+	#Extraemos la configuración del sistema del usuario/password
+	user=`extractParamBD 'user'`
+	password=`extractParamBD 'password'`
+	database=`extractParamBD 'db'`
+	#la información global del admin/password de mysql.
+	mysql_admin=`git config joomla.mysql-admin`
+	mysql_admin_pass=`git config joomla.mysql-password`
+	#Extraemos la información global de nuestro
+	######### Creando el usuario
+	echo -ne "Usuario ${light}$user${end}";
+	${MYSQL_CLIENT} --user=$mysql_admin --password=$mysql_admin_pass -e \
+	"CREATE USER '$user'@'localhost' IDENTIFIED BY '$password';"
+	echo -e " [Creado]";
+	#mostrando los usuarios
+	#${MYSQL_CLIENT} --user=$mysql_admin --password=$mysql_admin_pass mysql "SELECT DISTINCT( user.User ) from user";
+	######### Creando la BD
+	echo -ne "Creando Bse de datos ${light}$database${end}";
+	${MYSQL_CLIENT} --user=$mysql_admin --password=$mysql_admin_pass -e \
+	"CREATE DATABASE IF NOT EXISTS $database;"
+	echo -e " [Bien]";
+	
+	######### Otorgando privilegios
+	echo -ne "Otorgando privilegios de ${light}${database}${end} a ${light}${user}${end}";
+	${MYSQL_CLIENT} --user=$mysql_admin --password=$mysql_admin_pass -e "GRANT ALL PRIVILEGES ON $database.* TO '$user'@'localhost';"
+	echo -e " [Bien]";
+	######### Actualizando privilegios
+	echo -ne "Actualizando permisos";
+	${MYSQL_CLIENT} --user=$mysql_admin --password=$mysql_admin_pass -e "FLUSH PRIVILEGES;"
+	echo -e " [Bien]";
+}
+################################################################################
+# Categoria: Security
+# Revisa que todos los directorios en el repositorio no tienen el archivo
+# Index.html de no existir crea el archivo.
+################################################################################
+function existeIndexHTML()
+{
+	echo -e "${light}Creando archivos index${end}"
+	export count="0";
+	echo -ne "${yellow}${count}${end}\r";
+	find "${REPO_PATH}" -type d | grep -vE '\.git' | \
+	grep -vE 'sitio_joomla' | while read dir; do
+		if [ ! -f "${dir}/index.html" ]
+		then
+			export count=`expr  1 + $count`;
+			echo -ne "${yellow}${count}${end}\r";
+			touch "${dir}/index.html"
+		fi
+	done
+	echo -e "\t ${light}←${end} Archivos Creados";
+}
+##########################################################################################
+# Descricipcion: Borra todas las tablas de una BD MySQL                                  #
+# link: http://bash.cyberciti.biz/mysql/drop-all-tables-shell-script/                    #
+##########################################################################################
+function borrarTodasLasTablas()
+{
+	login=`extractParamBD 'user'`
+	password=`extractParamBD 'password'`
+	database=`extractParamBD 'db'`
+	for t in `${MYSQL_CLIENT} -u ${login} -p${password} ${database} -BNe 'show tables'`
+	do
+		echo -ne "${red}Borrando${end} -> ${yellow}${database}${end}.${light}${t}${end}"
+		echo -ne "                                                      \r"
+		${MYSQL_CLIENT} -u ${login} -p${password} -h localhost ${database} -e "drop table ${t}"
+	done
+}
+#########################################################################################
+function actualizaArchivo(){
+	local localName=$(echo $1)
+	#Extraemos el la ubicación del con directorio relativo al repositorio.
+	local fileDst=$(echo "${JOOMLA_DST}${localName}");
+	#Si el archivo no existe entonces significa que fue borrado
+	if [ ! -e "${REPO_PATH}/${localName}" ]
+	then
+		echo -ne "  → ${red}☹ Archivo Eliminado  :${end} ${localName}";
+		if [ -e "${fileDst}" ]
+		then
+			echo -ne "\t←--${red}[Borrando]${end}";
+			rm ${fileDst};
+		fi
+		echo "";
+	else
+		echo -ne "  → ${yellow}😁 Archivo Modificado :${end} ${localName}";
+		#Si la carpeta destino no existe la creamos.
+		local dirDst=$(dirname "${fileDst}")
+		if [ ! -d "${dirDst}" ]
+		then
+			echo -ne "${yellow}←--[Creando directorio]${end}"
+			echo "Directorio ${dirDst}";
+			mkdir -p "${dirDst}"
+		fi
+		cp -r "${REPO_PATH}/${localName}" "${fileDst}";
+		echo ''
+	fi
+}
+function actualizar_path()
+{
+	num_commit="1"
+	if [ $# -eq 1 ]
+	then
+		num_commit=${1}
+	fi
+	echo -e "${light}Backtraking sobre ${num_commit} confirmaciones${end}"
+	git log -1 --name-only --pretty=format:'' HEAD~${num_commit} | grep -E "^[^\s]" |\
+	grep -vE 'sitio_joomla' | while read localName; do
+		actualizaArchivo ${localName};
+	done
+	git status -s | sed  -r 's/^[^\s]+\s+//' | grep -vE 'sitio_joomla' |\
+	while read localName; do
+		actualizaArchivo ${localName};
+	done
+}
+##########################################################################################
+#      Actualiza la base de datos                                                        #
+##########################################################################################
+function actualizaBD()
+{
+	echo -e "${light}Actualizando Base de datos${end}"
+	bdFile=$(echo "${REPO_PATH}/sitio_joomla/bd.sql")
+	borrarTodasLasTablas
+	echo -e "  → ${red}Todas las tablas han sido borradas${end}                "
+	#Extraemos los parametros de la BD en cuestion
+	login=`extractParamBD 'user'`
+	password=`extractParamBD 'password'`
+	database=`extractParamBD 'db'`
+	for f in `find ${REPO_PATH}/sitio_joomla/ -iregex ".*sql$" | sort`
+	do
+		echo -ne "${light}Cargando datos...${end}\r";
+		${MYSQL_CLIENT} --default-character-set=utf8 -u ${login} -p${password} -h localhost ${database} < "${f}";
+	done;
+	echo -e "  → ${cyan}Los datos han sido actualizados correctamente${end}"
+}
+##########################################################################################
+#  Asigna permisos a los archivos                                                        #
+##########################################################################################
+function permisos()
+{
+	chmod 755 -R "${JOOMLA_DST}"
+}
+function mensajeDeAyuda()
+{
+echo -e "\t\t\t${light}Actualizador de proyectos Joomla${end}
+
+ ${light}Descripción${end}
+
+	Debe de configurar un par de parametros, para esto configura el directorio destino:
+		${cyan}git config joomla.dir-dst /opt/lampp/httpdocs/sportmall/${end}
+	En este caso hemos configurado el directorio destino en /opt/lampp/httpdocs/sportmall/
+	para ver si la configuracion es correcta puede usar el mismo comando pero sin arugmento de setado:
+		${cyan}git config joomla.dir-dst${end}
+	Debera aparecer el directorio que inserto el usuario:
+		 ${cyan}/opt/lampp/httpdocs/sportmall/${end}
+
+	De igual forma usted puede configurar la ruta de su propio cliente mysql, por ejemplo
+		${cyan}git config joomla.mysql-client /opt/lampp/etc/bin/mysql${end}
+	En caso de no configurar buscara el mysql-client que tenga instalado en su S.O.
+
+	Para instalar/borrar la bases de datos y usuarios automaticamente, es necesario agregar
+	usuario/password administrador de nuestro mysql p.e:${cyan}
+	
+		 git config --global joomla.mysql-admin 'root'
+		 git config --global joomla.mysql-password 'libertad'
+
+ ${light}Ejemplos de uso${end}
+	Creando la Base de datos, su usaurio, la ruta de archivos, etc...:
+	${cyan}git sitio-joomla -instalar${end}
+
+	Actualizando Simple(archivos sin seguimiento y modificados en el ultimo commit):
+	${cyan}git sitio-joomla${end}
+
+	Actualizando base de datos:
+	${cyan}git sitio-joomla -db${end}
+
+	Actualizando los archivos sin seguimiento y modificados hace 2 commits:
+	${cyan}git sitio-joomla -c2${end}
+
+	Actualizando base de datos, archivos sin seguimiento y modificados hace 5 commits:
+	${cyan}git sitio-joomla -bd -c5${end}
+
+	Esta página de ayuda:
+	${cyan}git sitio-joomla -ayuda${end}
+
+ ${light}Autor${end}
+	Fitorec
+";
+}
+if [ $# -gt 0 ]
+then
+	for arg in "$@"
+	do
+		case "${arg}" in
+		"-bd")
+				actualizaBD
+				;;
+		"-ayuda")
+				mensajeDeAyuda
+				;;
+		"-instalar")
+				installBD
+				actualizaBD
+				existeIndexHTML
+				actualizar_path
+				permisos;
+				;;
+		*)
+			if [ $? -eq 0 ]
+			then
+				existeIndexHTML
+				num_commit=$(echo "${arg}" | grep -oE '[0-9]+$');
+				actualizar_path ${num_commit}
+				permisos;
+			fi
+			;;
+		esac
+	done
+else
+	existeIndexHTML
+	actualizar_path 0
+	permisos;
+fi
